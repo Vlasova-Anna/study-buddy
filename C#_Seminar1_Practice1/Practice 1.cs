e55a4bf382a2a35e20cb33c587125bd39b7bfb89
AnnaVlasova <msannvlasova@mail.ru>
2023-07-19 09:46:58 +0300

    added info about Heading levels

--- C#_Seminar1_Practice1/Practice 1.cs
+++ C#_Seminar1_Practice1/Practice 1.cs
@@ -1,86 +1,9 @@
-// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
-// какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
 
-Console.WriteLine("Привет, хочешь узнать, какое из этих чисел больше?");
-Console.WriteLine("Введи первое число - ");
+Console.Write("Введите трёхзначное число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
 
-int A = Convert.ToInt32 (Console.ReadLine());
-
-Console.WriteLine("Введи второе число - ");
-
-int B = Convert.ToInt32(Console.Read());
-if(A > B)
-{
-Console.WriteLine("Первое число больше! Значит, второе -меньше.");
-}
-else
-{
-Console.WriteLine("Второе число больше! Значит, первое -меньше.");
-}
-
-
-
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-// 2, 3, 7 -> 7
-// 44 5 78 -> 78
-// 22 3 9 -> 22
-
-int max = 0;
-
-Console.WriteLine("Введите число");
-int a = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine("Введите число");
-int b = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine("Введите число");
-int c = Convert.ToInt32 (Console.ReadLine());
-
-max=a;
-if (b>max)
-{max=b;
-}
-if (c>max)
-{max=c;
-}
-int result= max;
-Console.WriteLine(result);
-
-
-
-// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
-//  является ли число чётным (делится ли оно на два без остатка).
-// 4 -> да
-// -3 -> нет
-// 7 -> нет
-
-Console.WriteLine("Введите число");
-int Num = Convert.ToInt32 (Console.ReadLine());
-
-if (Num%2 == 0)
-{
-Console.WriteLine("Ваше число чётное!");
-}
-else
-{
-Console.WriteLine("Ваше число нечётное!");
-}
-
-
-
-// Задача 8: Напишите программу, которая на вход принимает число (N), 
-// а на выходе показывает все чётные числа от 1 до N.
-// 5 -> 2, 4
-// 8 -> 2, 4, 6, 8
-
-Console.WriteLine("Введите число");
-int N = Convert.ToInt32 (Console.ReadLine());
-
-int startpoint = 1;
-while (startpoint < N)
-{
-Console.WriteLine(startpoint+1);
-startpoint+= 2;
-}
-
+int result= num1%100;
+int result1= num1%10;
+Console.WriteLine(result1);
--- Hello, world.md
+++ Hello, world.md
@@ -4,6 +4,21 @@
 
 ## Выделение текста:
 для создания некрупного заголовка/подзаголовка напечатайте в начале текста двойную решетку в англ раскладке (##) - автоматически добавится полоса отчерка заголовка/подзаголовка от основного текста.
+Величина заголовка зависит от количества решёток:
+
+Markdown	HTML	Rendered Output
+# Heading level 1	<h1>Heading level 1</h1>	
+Heading level 1
+## Heading level 2	<h2>Heading level 2</h2>	
+Heading level 2
+### Heading level 3	<h3>Heading level 3</h3>	
+Heading level 3
+#### Heading level 4	<h4>Heading level 4</h4>	
+Heading level 4
+##### Heading level 5	<h5>Heading level 5</h5>	
+Heading level 5
+###### Heading level 6	<h6>Heading level 6</h6>	
+Heading level 6
 
 *Курсив.* для этого обрамите текст одинарными звездочками
 
